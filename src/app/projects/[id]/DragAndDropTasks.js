2e87e187784ba51bb778ffbf020310837b23f6c4
Md. Jidanul Hakim Jitu <20-44365-3@student.aiub.edu>
2024-05-01 19:23:16 +0600

    drag and drop working without changing api

--- src/app/projects/[id]/DragAndDropTasks.js
+++ src/app/projects/[id]/DragAndDropTasks.js
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const DragAndDropTasks = ({ tasks, onStatusChange }) => {
@@ -18,8 +19,8 @@ const DragAndDropTasks = ({ tasks, onStatusChange }) => {
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
       <div className="flex">
-        {["todo", "in-progress", "done"].map((status) => (
-          <Droppable key={status} droppableId={status}>
+        {["todo", "in-progress", "done"].map((status, index) => (
+          <StrictModeDroppable key={status} droppableId={status}>
             {(provided) => (
               <div
                 {...provided.droppableProps}
@@ -32,7 +33,7 @@ const DragAndDropTasks = ({ tasks, onStatusChange }) => {
                   .map((task, index) => (
                     <Draggable
                       key={task.id}
-                      draggableId={task.id}
+                      draggableId={task.id.toString()}
                       index={index}
                     >
                       {(provided) => (
@@ -50,7 +51,7 @@ const DragAndDropTasks = ({ tasks, onStatusChange }) => {
                 {provided.placeholder}
               </div>
             )}
-          </Droppable>
+          </StrictModeDroppable>
         ))}
       </div>
     </DragDropContext>
@@ -58,3 +59,22 @@ const DragAndDropTasks = ({ tasks, onStatusChange }) => {
 };
 
 export default DragAndDropTasks;
+
+export const StrictModeDroppable = ({ children, ...props }) => {
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    const animation = requestAnimationFrame(() => setEnabled(true));
+
+    return () => {
+      cancelAnimationFrame(animation);
+      setEnabled(false);
+    };
+  }, []);
+
+  if (!enabled) {
+    return null;
+  }
+
+  return <Droppable {...props}>{children}</Droppable>;
+};
